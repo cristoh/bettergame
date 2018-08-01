94672ae738316be0d07854e34936e2e0873b35c2
apoplol <31998280+apoplol@users.noreply.github.com>
2018-08-01 15:00:45 -0400

    Add files via upload

---
+++ bettergame.py
@@ -0,0 +1,54 @@
+import random, re, time
+from rps import rips
+
+#variables
+#end variables
+
+#functions
+def getDirection():
+	choice = raw_input("choose a direction ")
+	return choice
+
+def moveLocation(choice):
+	if choice == "left" or choicels == "down":
+		return -1
+	elif choice == "right" or choice == "up":
+		return 1
+def generateRooms():
+	rooms = ["00","10","20","01","11","21","02","12","22"];
+	return rooms
+def findRoom(rooms,curx,cury):
+	roomFound = False
+	for room in rooms:
+		if curx == int(room[0]) and cury == int(room[1]):
+			roomFound = True
+	
+	return roomFound
+
+
+#end functions
+
+def game():
+	curx = 0
+	cury = 0
+	rooms = generateRooms()
+	roomFound = True
+	while True:
+		choice = getDirection()
+		if choice == "left" or choice == "right":
+			curx += moveLocation(choice)
+		else:
+			if choice == "up" or choice == "down":
+				cury += moveLocation(choice)
+
+		roomFound = findRoom(rooms,curx,cury)
+		if roomFound:
+			print "you are in a room"
+		else:
+			print "you hit a wall"
+
+		print "x = %s" % str(curx)
+		print "y = %s" % str(cury)
+rips()
+game()
+
---
+++ rps.py
@@ -0,0 +1,50 @@
+import random
+
+
+#functions
+def roll(choice):
+	won = 0
+	compRoll = genRoll()
+
+	if (choice == 1 and compRoll == 3) or (choice == 2 and compRoll == 1) or (choice == 3 and compRoll == 2):
+		hasWon = -1
+	elif choice == compRoll:
+		hasWon = 0
+	else:
+		hasWon = 1
+	return hasWon
+def genRoll():
+	compRoll = random.randrange(1,4)
+	print "The niglet rolled a %s " % compRoll 
+	return compRoll
+#end functions
+
+#varibles
+
+#end variables
+
+def rips():
+	options = [1,2,3]
+	choice = 0
+	hasWon = 0
+	myHealth = 100
+	hisHealth = 100
+	while True:
+		choice = int(raw_input("RPS! \n 1 beats 2, 2 beats 3, 3 beats 1 \n>"))
+		hasWon = roll(choice)
+		if hasWon == 1:
+			hisHealth -= 10
+			print "u win no health lost. moster has %s health left" % hisHealth
+		elif hasWon == -1:
+			myHealth -= 10
+			print "you have %s health left. monster has %s left" % (myHealth, hisHealth)
+		else:
+			print "stalemate!"
+
+		if myHealth <= 0:
+			print "youre ded"
+			return [0, hisHealth]
+		elif hisHealth <= 0:
+			print "he ded"
+			return [myHealth, 0]			
+	
